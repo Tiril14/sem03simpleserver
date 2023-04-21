5247da4c29f5d0b8228baa2dcde66fe5af219a3b
tirilkleppe <tirilk@uia.no>
2023-04-21 09:49:09 +0200

    mod

--- tcp_server.go
+++ tcp_server.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net"
 	"sync"
-	
+
 	"github.com/Tiril14/is105sem03/mycrypt"
 )
 
@@ -39,15 +39,14 @@ func main() {
 						return // fra for løkke
 
 					}
-					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n]))), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
 					log.Println("Dekrypter melding: ", string(dekryptertMelding))
-					switch msg := string(dekrypterMelding)
-					{
+					switch msg := string(dekryptertMelding); msg {
 
 					case "ping":
 						_, err = c.Write([]byte("pong"))
 					default:
-						_, err = c.Write(buf[:n])
+						_, err = c.Write([]byte(msg))
 					}
 					if err != nil {
 						if err != io.EOF {
